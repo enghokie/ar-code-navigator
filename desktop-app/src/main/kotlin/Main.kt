ec2a58b0615dac9d00c1332934e763fa55a0ab5b
Braedon Dickerson <braedon@paravision.ai>
2023-01-30 04:04:30 -0500

    feat(desktop): Generate basic code architecture for C++ code
    The basic class architecture for C++ source code is now generated from the
    OCR-converted text that's based on the images acquired by the program. The
    classes defined, referenced, and derived from will be tracked through the
    parsed source code.
    
    Once all the source code text files are traversed through, a final report will
    be logged to the output of the classes found, along with their parent classes
    and the other classes referenced within them.

--- desktop-app/src/main/kotlin/Main.kt
+++ desktop-app/src/main/kotlin/Main.kt
@@ -8,6 +8,7 @@ import java.util.*
 
 val EXTENSIONS_SUPPORTED = arrayOf<String>("png", "jpeg", "jpg", "tiff", "bmp")
 val OUTPUT_DIRECTORY = File("output")
+val CLASS_REGEX = "^class\\s+\\w+\\s+".toRegex()
 
 
 class OcrData (imgFile: File? = null, pixMap: PIX? = null, text: String? = null) {
@@ -16,6 +17,69 @@ class OcrData (imgFile: File? = null, pixMap: PIX? = null, text: String? = null)
     var text: String? = text
 }
 
+
+class ClassData (name: String) {
+    var name = name
+    var derivedClasses = HashSet<String>()
+    var refClasses = HashSet<String>()
+}
+
+
+class CodeData {
+    var classes = HashMap<String, ClassData>()
+}
+
+
+fun parseCode(codeText: String, codeData: CodeData): Boolean {
+    var curClass: ClassData? = null
+    val codeStrings = codeText.split('\n')
+    codestring@ for ((i, codeString) in codeStrings.withIndex()) {
+        var className = CLASS_REGEX.find(codeString)?.value?.drop("class".length)?.trim()
+        if (className !== null) {
+            // TODO: Account for nested classes
+            //if (curClass !== null) {
+            //    subCodeText = codeStrings.subList(i, codeStrings.lastIndex).joinToString("\n")
+            //    parseCode(subCodeText, codeData)
+            //    continue
+            //}
+
+            // Store class data details
+            if (!codeData.classes.containsKey(className)) {
+                codeData.classes[className] = ClassData(className)
+
+                // Check for derived classes
+                val derClassNameIdx = when {
+                    codeString.contains("public") -> codeString.indexOf("public") + "public".length + 1
+                    codeString.contains("private") -> codeString.indexOf("private") + "private".length + 1
+                    else -> -1
+                }
+                if (derClassNameIdx != -1) {
+                    val derClassName = codeString.substring(derClassNameIdx, codeString.indexOf(' ', derClassNameIdx))
+                    codeData.classes[className]?.derivedClasses?.add(derClassName)
+                }
+            }
+
+            curClass = codeData.classes[className]
+            continue
+        }
+        else if (curClass === null) {
+            // Can't process inner class code without knowing the class it belongs to...
+            continue
+        }
+
+        // Process inner class code
+        for (className in codeData.classes.keys) {
+            val classNameIdx = codeString.indexOf(className)
+            if (classNameIdx == -1) continue
+            else if (codeString[classNameIdx + className.length] == '(') continue@codestring
+
+            // TODO: Handle reference class objects whose class declaration hasn't been seen yet
+            if (codeString.contains(className)) curClass?.refClasses?.add(className)
+        }
+    }
+
+    return codeData.classes.isNotEmpty()
+}
 
 
 fun getPixMaps(path: String, ocrDataVec: Vector<OcrData>): Boolean {
@@ -66,7 +130,20 @@ fun main(args: Array<String>) {
         textFile.writeText(ocrData.text!!)
     }
 
+    // Parse and store class information from source code text
+    var codeData = CodeData()
+    for (ocrData in ocrDataVec) {
+        parseCode(ocrData.text?: "", codeData)
+    }
 
+    // Log class architecture
+    for (classData in codeData.classes.values) {
+        println("${classData.name} class details:")
+        println("\tParent classes:")
+        for (derClass in classData.derivedClasses) println("\t\t$derClass")
+        println("\tReference classes:")
+        for (refClass in classData.refClasses) println("\t\t$refClass")
+    }
 
     // Destroy used object and release memory
     tessApi.End()
