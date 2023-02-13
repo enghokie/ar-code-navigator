92ff4c578a57e284caa399da4aa039eb20e73007
Braedon Dickerson <braedon@paravision.ai>
2023-02-13 01:42:50 -0500

    feat(desktop): Support screen capturing mode
    Screen-captures can now be utilized instead of code file paths for OCR
    processing and generating the class hierarchy from the user's code present on
    the screen. To trigger this mode, use the CLI "screen" for the 1st positional
    parameter instead of the path to the source code files. Once this mode is
    triggered, press the "Enter" key on the keyboard to capture and process each
    subsequent screen capture desired.
    
    The processed screen capture results will be output to the same text and image
    files as the processed results from the previous mode does. The only difference
    in this mode is that the base name of the files will be "screen-code" followed
    by a numeric counter value starting at 1, and incrementing for each subsequent
    screen capture.

--- desktop-app/src/main/kotlin/Main.kt
+++ desktop-app/src/main/kotlin/Main.kt
@@ -174,38 +174,77 @@ fun main(args: Array<String>) {
     }
 
     var codeData = CodeData()
+    if (source == "screen") {
+        // Initialize the Robot package object for screen capturing
+        val robot = Robot()
+        val javaFrameConverter = Java2DFrameConverter()
+        val leptFrameConverter = LeptonicaFrameConverter()
+
+        // Set up the output code text directory from the parsed images
+        OUTPUT_DIRECTORY = File(OUTPUT_DIRECTORY.name + "/screen")
+        if (!OUTPUT_DIRECTORY.exists()) OUTPUT_DIRECTORY.mkdir()
+
+        // Capture the screen images and process the code text found
+        var imgCount = 0
+        while (true) {
+            // Capture the whole screen
+            val screenArea = Rectangle(Toolkit.getDefaultToolkit().screenSize)
+            val screenImgBuffer = robot.createScreenCapture(screenArea)
+            val pixMap = leptFrameConverter.convert(javaFrameConverter.convert(screenImgBuffer))
+            imgCount += 1
+
+            // Produce the code text with OCR
+            val codeText = processOcr(tessApi, pixMap)
+
+            // Save the text from this image to a file in the current output directory
+            val textFile = File(OUTPUT_DIRECTORY.path + "/screen-code$imgCount" + extension)
+            textFile.writeText(codeText)
+
+            // Save the Tesseract pre-processed image to the current output directory
+            val preprocessedImage = tessApi.GetThresholdedImage()
+            pixWritePng("${textFile.parentFile.path}/${textFile.nameWithoutExtension}.png", preprocessedImage, 1.0f)
+
+            // Parse and process the code
+            if (parseCode(language, codeText, codeData)) logClassHiararchy(codeData)
+
+            // No longer need this pixel data
+            pixMap.deallocate()
+
+            println("Press enter to capture again...")
+            readLine()
         }
     }
+    else {
+        // Get pixel maps from images
+        var ocrDataVec = Vector<OcrData>()
+        if (!getPixMaps(source, ocrDataVec))
+            throw Exception("No images found in the provided path: $source")
 
-    // Get pixel maps from images
-    var ocrDataVec = Vector<OcrData>()
-    if (!getPixMaps(source, ocrDataVec))
-        throw Exception("No images found in the provided path: $source")
+        // Perform OCR on pixel maps to convert them to text
+        for (ocrData in ocrDataVec) {
+            ocrData.text = processOcr(tessApi, ocrData.pixMap!!)
 
-    // Perform OCR on pixel maps to convert them to text
-    for (ocrData in ocrDataVec) {
-        ocrData.text = processOcr(tessApi, ocrData.pixMap!!)
+            // Save the text from this image to a file in the current directory
+            val outputDir = File(OUTPUT_DIRECTORY.name + "/${ocrData.imgFile?.parentFile?.name}")
+            if (!outputDir.exists()) outputDir.mkdir()
+            val textFile = File(outputDir.path + "/" + ocrData.imgFile?.nameWithoutExtension + extension)
+            textFile.writeText(ocrData.text!!)
 
-        // Save the text from this image to a file in the current directory
-        val outputDir = File(OUTPUT_DIRECTORY.name + "/${ocrData.imgFile?.parentFile?.name}")
-        if (!outputDir.exists()) outputDir.mkdir()
-        val textFile = File(outputDir.path + "/" + ocrData.imgFile?.nameWithoutExtension + extension)
-        textFile.writeText(ocrData.text!!)
+            // Save the Tesseract pre-processed image to the current output directory
+            val preprocessedImage = tessApi.GetThresholdedImage()
+            pixWritePng("${textFile.parentFile.path}/${textFile.nameWithoutExtension}.png", preprocessedImage, 1.0f)
 
-        // Save the Tesseract pre-processed image to the current output directory
-        val preprocessedImage = tessApi.GetThresholdedImage()
-        pixWritePng("${textFile.parentFile.path}/${textFile.nameWithoutExtension}.png", preprocessedImage, 1.0f)
+            // No longer need this pixel data
+            ocrData.pixMap?.deallocate()
+        }
 
-        // No longer need this pixel data
-        ocrData.pixMap?.deallocate()
+        // Parse and store class information from source code text
+        for (ocrData in ocrDataVec) {
+            parseCode(language, ocrData.text!!, codeData)
+        }
+
+        logClassHiararchy(codeData)
     }
-
-    // Parse and store class information from source code text
-    for (ocrData in ocrDataVec) {
-        parseCode(language, ocrData.text!!, codeData)
-    }
-
-    logClassHiararchy(codeData)
 
     // Destroy used object and release memory
     tessApi.End()
